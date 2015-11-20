f8296b1fb692fc945e4c7c83ffbb123a4fb00acd
galaipa <bingalipa@gmail.com>
2015-11-20 19:45:46 +0100

    Javadoc-a gehitu

--- src/io/github/galaipa/GEAPI.java
+++ src/io/github/galaipa/GEAPI.java
@@ -18,21 +18,42 @@ public class GEAPI {
     public GEAPI(GameErauntsiaMC p) {
         this.plugin = p;
     }
+
+    /**
+     * Estatistika parametro bat kargatu edo sortu
+     * @param s Kargatu nahi den estatistikaren izena
+     */
     public void kargatuStat(String s){
         statsAPI.addStat(new PointStat(s));
     }
+
+    /**
+     * Estatistika bat eguneratu
+     * @param s Estatistika parametroaren izena
+     * @param in Zenbat gehitu nahi zaion
+     * @param p Jokalaria
+     */
     public void gehituStat(String s , Integer in,Player p){
         Stat stat = statsAPI.getStatManager().getStat(s);
         StatsHolder holder = statsAPI.getPlayer(p.getUniqueId());
         holder.addEntry(stat, new DefaultStatEntry(1,new MetadataPair("world", p.getLocation().getWorld().getName())));
-    }public static String infoStat(String s,Player p){
+    }
+
+    /**
+     * Jokalari batek estatistika batean duen balioa eskuratu
+     * @param s Estatistika parametroaren izena
+     * @param p Jokalaria
+     * @return Balioa String moduan
+     */
+    public static String infoStat(String s,Player p){
         Stat stat = statsAPI.getStatManager().getStat(s);
         for (StatEntry e : statsAPI.getPlayer(p.getUniqueId()).getStats(stat)){
             DecimalFormat df = new DecimalFormat("###");
             return df.format(e.getValue());
         }
-        return "0";
-    }public String checkok(){
+        return "";
+    }
+    public String checkok(){
         return "OK!";
     }
 }
